0bbe884e1fc950fa4d00b909d123cd2491779f28
kz1021 <kristina.zimina21@imperial.ac.uk>
2024-11-20 00:00:13 +0000

    Test Qwen model using the data set

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,5 @@
 from .mistral_utils import mistral_ask
 from .local_llama_utils import llama_instruct_ask
 from .local_roberta_utils import roberta_ask
+from .local_qwen_utils import qwen_ask
+from .local_gemma_utils import gemma_ask
--- utils/local_qwen_utils.py
+++ utils/local_qwen_utils.py
@@ -9,25 +9,24 @@ model = AutoModelForCausalLM.from_pretrained(
 )
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-prompt = "Give me a short introduction to large language model."
-messages = [
-    {"role": "system", "content": "You are Qwen, created by Alibaba Cloud. You are a helpful assistant."},
-    {"role": "user", "content": prompt}
-]
-text = tokenizer.apply_chat_template(
-    messages,
-    tokenize=False,
-    add_generation_prompt=True
-)
-model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
+def qwen_ask(context, prompt):
+    messages = [
+        {"role": "system", "content": f"You know that {context}."},
+        {"role": "user", "content": f"Is it true that {prompt}?"}
+    ]
+    text = tokenizer.apply_chat_template(
+        messages,
+        tokenize=False,
+        add_generation_prompt=True
+    )
+    model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
 
-generated_ids = model.generate(
-    **model_inputs,
-    max_new_tokens=512
-)
-generated_ids = [
-    output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
-]
+    generated_ids = model.generate(
+        **model_inputs,
+        max_new_tokens=512
+    )
+    generated_ids = [
+        output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+    ]
 
-response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
-print(response)
+    return tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
